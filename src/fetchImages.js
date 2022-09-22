380a5c2ae9f15e6ffa6ab5e6285643c757de69a9
Darya <HavrylenkoIT@gmail.com>
2022-09-23 00:20:37 +0300

    рендер розмітки

---
+++ src/fetchImages.js
@@ -0,0 +1,22 @@
+import axios from "axios"
+
+let searchQuery = '';
+let page = 1;
+async function fetchImages(searchQuery) {
+    const { data } = await axios.get(`https://pixabay.com/api/?key=30087665-92eb3edde2a629aded169ee28&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`);
+    incrementPage();
+    return data;
+}
+
+
+
+function incrementPage() {
+    page += 1;
+}
+function resetPage() {
+    page = 1;
+}
+
+
+
+export { fetchImages, resetPage};
--- src/index.js
+++ src/index.js
@@ -1,2 +1,58 @@
 import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import Notiflix from 'notiflix';
+import { fetchImages, resetPage } from "./fetchImages";
+
+
+const searchForm = document.querySelector('.search-form');
+
+const input = document.querySelector('input');
+
+const btnSubmit = document.querySelector('button');
+const galleryItems = document.querySelector('.gallery');
+const btnLoadMore = document.querySelector(".load-more");
+
+searchForm.addEventListener('submit', onSearch);
+btnLoadMore.addEventListener('click', onLoadMore)
+
+
+
+  function onSearch(e) {
+    e.preventDefault();
+    searchQuery = e.currentTarget.elements.searchQuery.value;
+    resetPage();
+    fetchImages(searchQuery).then(r => renderMarkUp(r)).catch(err => console.log(err));
+    
+    }
+ 
+
+     function renderMarkUp(r) {
+const markUp = r.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
+  return `<div class="photo-card">
+    <a class="gallery__item" href="${largeImageURL}">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  </a>
+  <div class="info">
+  <p class="info-item">
+      <b>Likes<span class="likes">${likes}</span></b>
+    </p>
+    <p class="info-item">
+      <b>Views<span class="views">${views}</span></b>
+    </p>
+    <p class="info-item">
+      <b>Comments<span class="comments">${comments}</span></b>
+    </p>
+    <p class="info-item">
+      <b>Downloads<span class="downloads">${downloads}</span></b>
+    </p>
+  </div>
+</div>`;
+}).join('');
+galleryItems.innerHTML = markUp;
+}
+
+function onLoadMore() {
+fetchImages(searchQuery);
+}
+
+
